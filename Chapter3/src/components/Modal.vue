ae08e64707c0d801a74e3b805961f91173cd2ac8
Behlül UYSAL <mockingjaytes@gmail.com>
2022-02-21 09:45:37 +0300

    Added Chapter3

---
+++ Chapter3/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+      <button @click="closeModal">Close Modal</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+      closeModal() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  h1 {
+    color: green;
+  }
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+</style>
